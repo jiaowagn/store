2ee44c21f90e8f77f54a075cd77b6a568fee748c
jiaowagn <wangjiaohere@gmail.com>
2018-05-10 16:41:36 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,11 @@
 class CartItemsController < ApplicationController
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
+  end
+
   def destroy
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
@@ -6,5 +13,10 @@ class CartItemsController < ApplicationController
     @cart_item.destroy
     flash[:warning] = "成功将 #{@product.title} 从购物车删除！"
     redirect_back(fallback_location: carts_path)
+  end
+
+  protected
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
