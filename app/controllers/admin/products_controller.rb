535554832a0513658bb8e7f9764a3ee8286efdbe
jiaowagn <wangjiaohere@gmail.com>
2018-05-10 12:35:05 +0800

    Implement admin::products CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,47 @@
+class Admin::ProductsController < ApplicationController
+  before_action :find_product, :only => [:show, :edit, :update, :destroy]
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
+  protected
+    def find_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :description, :price, :quantity)
+    end
+end
