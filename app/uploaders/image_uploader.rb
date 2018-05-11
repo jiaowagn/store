14330a8f1599129adac1274a2d21c2bed2084ee5
jiaowagn <wangjiaohere@gmail.com>
2018-05-11 15:23:05 +0800

    add qiniu & figaro

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.production?
+    storage :qiniu
+  end 
 
   process resize_to_fit: [800, 800]
 
